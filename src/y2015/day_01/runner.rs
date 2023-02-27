8b1649707d34a26044ca3334c3db58feee0c0549
Cesar Palacios <cesarlp.ng@gmail.com>
2023-02-27 20:18:59 -0300

    01/xx/2015

--- src/y2015/day_01/runner.rs
+++ src/y2015/day_01/runner.rs
@@ -1,7 +1,38 @@
+use std::fs;
+
 pub fn run_part_one() {
-    println!("Hello from part one!");
+    let contents = fs::read_to_string("src/y2015/day_01/input.txt").unwrap();
+
+    let mut _floor = 0;
+
+    for char in contents.chars() {
+        if char == '(' {
+            _floor += 1;
+        } else if char == ')' {
+            _floor -= 1;
+        }
+    }
 }
 
 pub fn run_part_two() {
-    println!("Hello from part two!");
+    let contents = fs::read_to_string("src/y2015/day_01/input.txt").unwrap();
+    let chars_length = contents.chars().count();
+    let mut chars_iter = contents.chars();
+
+    let mut floor = 0;
+    let mut i = 0;
+
+    while floor != -1 && i < chars_length {
+        if let Some(ch) = chars_iter.next() {
+            if ch == '(' {
+                floor += 1;
+            } else if ch == ')' {
+                floor -= 1;
+            }
+
+            i += 1;
+        }
+    }
+
+    let _basement_pos = if floor == -1 { i } else { 0 };
 }
