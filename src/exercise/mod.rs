7023bc9ab24f1bec1be71eaaf557c75a36c5679c
Cesar Palacios <cesarlp.ng@gmail.com>
2023-12-16 17:55:20 -0300

    08/2023

--- src/exercise/mod.rs
+++ src/exercise/mod.rs
@@ -4,4 +4,4 @@ mod utils;
 
 pub use day_exercise::DayExercise;
 pub use exercises::get;
-pub use utils::{concatenate_str, is_vowel, concatenate};
+pub use utils::{concatenate, concatenate_str, is_vowel, lcm, lcm_of};
--- src/exercise/utils.rs
+++ src/exercise/utils.rs
@@ -31,3 +31,37 @@ pub fn concatenate(input: Vec<u32>) -> u64 {
         .iter()
         .fold(0, |acc: u64, elem: &u32| acc * 10 + (*elem as u64));
 }
+
+pub fn lcm_of(input: Vec<u64>) -> u64 {
+    let mut total = 1;
+
+    input.into_iter().for_each(|value| {
+        total = lcm(total, value);
+    });
+
+    return total;
+}
+
+pub fn lcm(first: u64, second: u64) -> u64 {
+    first * second / gcd(first, second)
+}
+
+fn gcd(first: u64, second: u64) -> u64 {
+    let mut max = first;
+    let mut min = second;
+    if min > max {
+        let val = max;
+        max = min;
+        min = val;
+    }
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
